4986142d906b4e7f6edc7cdc5ba669b655899e38
Tanvi-Jain01 <123053700+Tanvi-Jain01@users.noreply.github.com>
2023-05-14 15:49:42 +0530

    Update interpolation.py

--- interpolation.py
+++ interpolation.py
@@ -30,14 +30,15 @@ dataset_file = wget.download(dataset_url)
 ds = xr.open_dataset(dataset_file)
 
 # Display the dataset
-st.write(ds)
+st.write(type(ds))
 
 
 df = ds.to_dataframe().reset_index().set_index("time")
-df = df["2022-01-01": "2022-12-31"]
-print(type(df))
+
 df["datetime"] = df.index
 
-
+df = df["2022-01-01": "2022-12-31"]
+print(type(df))
+st.dataframe(df)
 
 st.write("Hello")
